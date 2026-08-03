2fbfe08190e26cff01cabaf4894836e7210f3ca3
Ankit Tiwari <tiwariankit0503@gmail.com>
2026-08-03 20:59:32 +0530

    add link

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/auth",
+  baseURL: "https://taskflow-backend-t51m.onrender.com/api/auth",
 });
 
 export const registerUser = (data) =>
--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://taskflow-backend-t51m.onrender.com/api/tasks",
 });
 
 // Attach JWT Token if available
